855c2b009f5e3cd38ac1ab3d7b04745cb22fd574
Richard Mattinson <richard.m@ttinson.net>
2022-10-19 12:23:23 +0100

    product grid state and component

--- client/src/components/Products/ProductGrid.js
+++ client/src/components/Products/ProductGrid.js
@@ -1,5 +1,23 @@
-const ProductGrid = () => {
-  return (<>Product Grid</>);
+// import { useState } from "react";
+
+const ProductGrid = ({product}) => {
+  // const [product, setProduct] = useState([
+  //   { name: "Guinea Pig Pencil Case", category: "Pencil Cases", id: 1 },
+  //   { name: "Penguin Face Mask", category: "Face Masks", id: 2 },
+  //   { name: "Duckling Coin Purse", category: "Coin Purses", id: 3 },
+  // ]);
+  return (
+  <>
+    <div className="container m-100">
+      {product.map((product) => (
+        <div className="product-card" key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.category}</p>
+        </div>
+      ))}
+    </div>
+  </>
+  );
 };
 
 export default ProductGrid;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,20 @@
-import HomeCarousel from "../components/Home/HomeCarousel"
+import { useState } from "react";
+
+import HomeCarousel from "../components/Home/HomeCarousel";
 import HomeWelcome from "../components/Home/HomeWelcome";
 import ProductGrid from "../components/Products/ProductGrid";
 
 const Home = () => {
+    const [product, setProduct] = useState([
+      { name: "Guinea Pig Pencil Case", category: "Pencil Cases", id: 1 },
+      { name: "Penguin Face Mask", category: "Face Masks", id: 2 },
+      { name: "Duckling Coin Purse", category: "Coin Purses", id: 3 },
+    ]);
   return (
     <>
-    <HomeCarousel />
-    <HomeWelcome />
-    <ProductGrid />
+      <HomeCarousel />
+      <HomeWelcome />
+      <ProductGrid product={product}/>
 
       <div className="container m-100">
         <p>
